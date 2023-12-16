f1641c2de7503bd3b0cd8cce38b4a87653de271d
Karan Mamaniya <kmamaniya@gmail.com>
2023-12-16 14:21:59 -0800

    Add the poetry - unblinking

--- script.js
+++ script.js
@@ -6,23 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
             title: "My First Blog Post",
             date: "December 14, 2023",
             content: "This is the content of my first blog post. I'm learning to create a blog using JavaScript!"
-        },
-        {
-            title: "My Second Blog Post",
-            date: "December 15, 2023",
-            content: "This is my second post. Today I learned about arrays in JavaScript!"
         }
         // Add more blog posts here
     ];
 
     const poetryPosts = [
         {
-            title: "My First Poem",
+            title: "Unblinking",
             date: "December 16, 2023",
             lines: [
-                "The first line of my poem",
-                "The second line, a continuation",
-                "A final line to close"
+                "His eyes draw me in,",
+                "fringed with lashes as fine as spiders’ silk,",
+                "plush as an otter’s coat.",
+                "Comparisons of the colors to chocolate",
+                "and depth to the fathoms below",
+                "ring hollow and cliché.",
+                "They sing with the same melody",
+                "as the shadows in a redwood forest,",
+                "sheltering pockets of mist",
+                "and clutches of sweet sorrel.",
+                "They harbor something ancient,",
+                "yet something untouched by lifetimes lost."
             ]
         },
         // Add more poetry posts here
