df6af84c08b54f580c79418b68eb59f3e9dd1c57
Karan Mamaniya <kmamaniya@gmail.com>
2023-12-16 12:25:48 -0800

    Add logo and poems

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const postContainer = document.getElementById('blog-post');
+    const postContainer = document.getElementById('blog-posts');
 
-    const blogPost = {
-        title: "My First Blog Post",
-        date: "December 14, 2023",
-        content: "This is the content of my first blog post. I'm learning to create a blog using JavaScript!"
-    };
+    const blogPosts = [
+        {
+            title: "My First Blog Post",
+            date: "December 14, 2023",
+            content: "This is the content of my first blog post. I'm learning to create a blog using JavaScript!"
+        },
+        {
+            title: "My Second Blog Post",
+            date: "December 15, 2023",
+            content: "This is my second post. Today I learned about arrays in JavaScript!"
+        }
+        // Add more blog posts here
+    ];
 
-    const postHtml = `
-        <h2>${blogPost.title}</h2>
-        <p><em>Posted on: ${blogPost.date}</em></p>
-        <p>${blogPost.content}</p>
-    `;
+    const poetryPosts = [
+        {
+            title: "My First Poem",
+            date: "December 16, 2023",
+            lines: [
+                "The first line of my poem",
+                "The second line, a continuation",
+                "A final line to close"
+            ]
+        },
+        // Add more poetry posts here
+    ];
 
-    postContainer.innerHTML = postHtml;
+    // Function to format the date for sorting
+    function formatDateForSort(dateStr) {
+        return new Date(dateStr).getTime();
+    }
+
+     // Combine and sort the posts
+     const combinedPosts = blogPosts.concat(poetryPosts.map(post => ({...post, isPoetry: true})))
+     .sort((a, b) => formatDateForSort(a.date) - formatDateForSort(b.date));
+
+    // Function to display posts
+    function displayPost(post) {
+        let contentHtml = post.isPoetry ? post.lines.join('<br>') : post.content;
+        const postHtml = `
+            <div class="${post.isPoetry ? 'poetry-post' : 'blog-post'}">
+                <h2>${post.title}</h2>
+                <p><em>Posted on: ${post.date}</em></p>
+                <p>${contentHtml}</p>
+            </div>
+        `;
+        postContainer.innerHTML += postHtml;
+    }
+
+    // Display each post
+    combinedPosts.forEach(displayPost);
 });
